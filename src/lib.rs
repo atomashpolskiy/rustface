ae89c048f6a5eb15fcec7264765595d04a0e50f7
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-12-18 18:16:50 +0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub struct FeatureMap {
     // LABFeatureMap specifics
     rect_width: u32,
     rect_height: u32,
+    num_rect: u32,
 }
 
 impl FeatureMap {
@@ -27,6 +28,7 @@ impl FeatureMap {
             square_int_img: vec![],
             rect_width: 0,
             rect_height: 0,
+            num_rect: 0,
         }
     }
 
@@ -37,6 +39,8 @@ impl FeatureMap {
 
         self.reshape(width, height);
         self.compute_integral_images(input);
+        self.compute_rect_sum();
+        self.compute_feature_map();
     }
 
     fn reshape(&mut self, width: u32, height: u32) {
@@ -88,6 +92,62 @@ impl FeatureMap {
                 math::vector_sub(bottom_right.offset(1), top_right.offset(1), dest, width);
                 math::vector_sub(dest, bottom_left, dest, width);
                 math::vector_add(dest, top_left, dest, width);
+            }
+        }
+    }
+
+    fn compute_feature_map(&mut self) {
+        let width = self.width - self.rect_width * self.num_rect;
+        let height = self.height - self.rect_height * self.num_rect;
+        let offset = self.width * self.rect_height;
+
+        let feat_map_ptr = self.feat_map.as_mut_ptr();
+
+        unsafe {
+            for r in 0..height {
+                for c in 0..width {
+                    let mut dest = feat_map_ptr.offset((r * self.width + c) as isize);
+                    *dest = 0;
+
+                    let white_rect_sum = self.rect_sum[((r + self.rect_height) * self.width + c + self.rect_width) as usize];
+
+                    let mut black_rect_idx = r * self.width + c;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x80
+                    };
+
+                    black_rect_idx += self.rect_width;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x40
+                    };
+                    black_rect_idx += self.rect_width;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x20
+                    };
+
+                    black_rect_idx += offset;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x08
+                    };
+                    black_rect_idx += offset;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x01
+                    };
+
+                    black_rect_idx -= self.rect_width;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x02
+                    };
+                    black_rect_idx -= self.rect_width;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x04
+                    };
+
+                    black_rect_idx -= offset;
+                    if white_rect_sum >= self.rect_sum[black_rect_idx as usize] {
+                        *dest |= 0x10
+                    };
+                }
             }
         }
     }
