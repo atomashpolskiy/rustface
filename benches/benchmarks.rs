466a0ed8853842b30fa706d264d02c8f8e825d12
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-11-25 10:25:52 +0100

    Partial revert of 5752d7e78c5aa391549741a29b4f542ac43877d0

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -43,7 +43,7 @@ fn get_default_detector() -> Box<dyn Detector> {
 
 fn detect_single_image(c: &mut Criterion) {
     let mut detector = get_default_detector();
-    let img = get_test_image().to_luma8();
+    let img = get_test_image().to_luma();
 
     // convert to rustface internal image datastructure
     let (width, height) = img.dimensions();
